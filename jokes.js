e3030dcd32dd43c628edb260bc31c2f37b77f27c
fabianfv <fabianfloresvadell@gmail.com>
2021-04-14 17:45:09 +0200

    web design done, jQuery and fetch AJAX requests working

--- jokes.js
+++ jokes.js
@@ -1,23 +1,34 @@
-const btnjQueryAJAX = document.getElementById("btnjQueryAJAX");
-const btnFetchAJAX = document.getElementById("btnFetchAJAX");
+const btnjQueryAJAX = document.getElementById("btnjQueryAJAX")
+const btnFetchAJAX = document.getElementById("btnFetchAJAX")
+const jQueryViewer = document.getElementById("jQueryViewer")
+const fetchViewer = document.getElementById("fetchViewer")
+const apiURL = "http://api.icndb.com/jokes/random"
 
-btnjQueryAJAX.onclick = jQueryAJAXHandler;
-btnFetchAJAX.onclick = fetchAJAXHandler;
+btnjQueryAJAX.onclick = getJokeUsingjQuery
+btnFetchAJAX.onclick = getJokeUsingFetch
 
-function jQueryAJAXHandler() {
-  const joke = getJokeUsingjQuery();
+const jQueryWrite = joke => jQueryViewer.textContent = joke.replaceAll("&quot;", '"')
+const fetchWrite = joke => fetchViewer.textContent = joke.replaceAll("&quot;", '"')
 
-}
-
-function fetchAJAXHandler() {
-  const joke = getJokeUsingFetch();
-}
-
+//This api doesn't seem to return any errors
 function getJokeUsingjQuery() {
-
+  /*
+  $.ajax({
+    url: apiURL,
+    type: "get",
+    dataType: "json",
+    success: data => jQueryWrite(data.value.joke)
+  });
+  */
+  $.get(apiURL, data => jQueryWrite(data.value.joke))
 }
 
 function getJokeUsingFetch() {
-
+  fetch(apiURL)
+    .then(data => (data.ok ? data.json() : Promise.reject(data)))
+    .then(data => fetchWrite(data.value.joke))
+    .catch(err => console.log("Se produjo un error: ", err))
 }
 
+getJokeUsingjQuery()
+getJokeUsingFetch()
