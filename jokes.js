c077e9d2555387499f9fb01ebbb28b0f5d3706cd
fabianfv <fabianfloresvadell@gmail.com>
2021-04-15 16:31:51 +0200

    add some refactors to catch some possible network errors and comments

--- jokes.js
+++ jokes.js
@@ -7,8 +7,17 @@ const apiURL = "https://api.icndb.com/jokes/random"
 btnjQueryAJAX.onclick = getJokeUsingjQuery
 btnFetchAJAX.onclick = getJokeUsingFetch
 
+/*
 const jQueryWrite = joke => jQueryViewer.textContent = joke.replaceAll("&quot;", '"')
 const fetchWrite = joke => fetchViewer.textContent = joke.replaceAll("&quot;", '"')
+*/
+function writeToDOM(htmlElement, joke) {
+  try {
+    htmlElement.textContent = joke.replaceAll("&quot;", '"');
+  } catch (err) {
+    htmlElement.textContent = err;
+  }
+}
 
 //This api doesn't seem to return any errors
 function getJokeUsingjQuery() {
@@ -17,17 +26,30 @@ function getJokeUsingjQuery() {
     url: apiURL,
     type: "get",
     dataType: "json",
+    error: (xhr, textStatus, error) => jQueryWrite(`${textStatus:} ${error}`),
     success: data => jQueryWrite(data.value.joke)
   });
   */
-  $.get(apiURL, data => jQueryWrite(data.value.joke))
+  
+  /* $.get y $.get.JSON son abreviaturas de $.ajax
+  *  La diferencia entre estos 2 metodos es que $.get devuelve la respuesta en su formato original
+  *  mientras que $.getJSON intenta convertir el formato a JSON
+  */
+  
+  // En este caso se usa el método $.get con una función callback, pero también se pueden usar
+  // los métodos .done, .fail, .always (.done y .fail: promesas)
+  //$.get(apiURL, data => jQueryWrite(data.value.joke));
+  $.getJSON(apiURL)
+    .done(data => writeToDOM(jQueryViewer, data.value.joke))
+    .fail(err => writeToDOM(jQueryViewer, err));
+
 }
 
 function getJokeUsingFetch() {
   fetch(apiURL)
     .then(data => (data.ok ? data.json() : Promise.reject(data)))
-    .then(data => fetchWrite(data.value.joke))
-    .catch(err => console.log("Se produjo un error: ", err))
+    .then(data => writeToDOM(fetchViewer, data.value.joke))
+    .catch(err => writeToDOM(fetchViewer, err));
 }
 
 getJokeUsingjQuery()
